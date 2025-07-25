655ca44dafd1f14d7b9fbae25795df49fc845aa3
Harry Hernandez Arias <132928917+HarryHndz@users.noreply.github.com>
2025-07-25 12:57:05 -0600

    schema de login con yup

---
+++ validation/loginValidation.ts
@@ -0,0 +1,10 @@
+import { ILogin } from '@/data/ILogin'
+import * as yup from 'yup'
+
+export const loginValidation = ():yup.ObjectSchema<ILogin>=>{
+  const validationSchema:yup.ObjectSchema<ILogin> = yup.object().shape({
+    email: yup.string().email('Correo inválido').required('Correo es requerido'),
+    password: yup.string().required('Contraseña es requerida'),
+  })
+  return validationSchema
+}
